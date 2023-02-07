0ba673b00e4f61e8cba3f0cd3438f20ffe0093e7
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-07 19:04:28 +0600

    added eff solution

--- src/letter-comb-phone.ts
+++ src/letter-comb-phone.ts
@@ -38,3 +38,39 @@ console.time("combinationSum");
 const res = letterCombinations("234");
 console.log(res);
 console.timeEnd("combinationSum");
+
+/* function letterCombinations(digits: string): string[] {
+  if (!digits) {
+      return [];
+  }
+
+  const dict: { [key: string]: string[] } = {
+      '1': null,
+      '2': ['a', 'b', 'c'],
+      '3': ['d', 'e', 'f'],
+      '4': ['g', 'h', 'i'],
+      '5': ['j', 'k', 'l'],
+      '6': ['m', 'n', 'o'],
+      '7': ['p', 'q', 'r', 's'],
+      '8': ['t', 'u', 'v'],
+      '9': ['w', 'x', 'y', 'z']
+  };
+
+  let res: string[] = [''];
+
+  for (const c of digits) {
+      const list = dict[c];
+      if (!list) {
+          continue;
+      }
+      const temp: string[] = [];
+      for (const l of list) {
+          for (const str of res) {
+              temp.push(str + l);
+          }
+      }
+      res = temp;
+  }
+
+  return res;
+} */
