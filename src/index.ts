ea985a46bd732f3ba32ae7786beb8a076767791f
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-07 18:57:14 +0600

    solved letter combination of phone

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
-function combinationSum(candidates: number[], target: number) {
-  const dfs = (i: number, currentCandidates: number[], total: number) => {
-    if (target === total) {
-      result.push([...currentCandidates]);
-      return;
+const dict = {
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz"
+};
+
+function letterCombinations(digits: string): string[] {
+  if (digits === "") return [];
+  const digitsArr = digits.split("").reverse();
+  const digitsLength = digitsArr.length;
+  if (digitsArr.length === 1) return Array.from(dict[+digitsArr.pop()]);
+
+  function recursive(num1: string[], num2: string[]) {
+    if (!num2) return [];
+
+    const arr1 = Array.from(num1);
+    const arr2 = Array.from(num2);
+
+    const newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        newArr.push(arr1[i] + arr2[j]);
+      }
     }
-    if (i >= candidates.length || total > target) return;
-
-    currentCandidates.push(candidates[i]);
-    dfs(i, currentCandidates, total + candidates[i]);
-    currentCandidates.pop();
-    dfs(i + 1, currentCandidates, total);
-
-    return;
-  };
-
-  const result = [];
-
-  dfs(0, [], 0);
-
-  return result;
+    return newArr.concat(recursive(newArr, dict[+digitsArr.pop()]));
+  }
+  return recursive(dict[+digitsArr.pop()], dict[+digitsArr.pop()]).filter(
+    (el: string) => el.length === digitsLength
+  );
 }
 
 console.time("combinationSum");
-const res = combinationSum([2, 3, 6, 7], 7);
+const res = letterCombinations("234");
 console.log(res);
 console.timeEnd("combinationSum");
