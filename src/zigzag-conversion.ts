c24d15dd3eb29c699dd47b682e71c204d120784d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-14 19:14:05 +0600

    implemented  zigzag convert

--- src/zigzag-conversion.ts
+++ src/zigzag-conversion.ts
@@ -1,4 +1,28 @@
-export function convert(s: string, numRows: number): string {
-  return;
+export function convert(s: string, numRows: number) {
+  if (numRows == 1) return s;
+
+  // let sections = Math.ceil(n / (2 * numRows - 2.0));
+  // let numCols = sections * (numRows - 1);
+
+  const matrix = Array.from({ length: numRows }, () => []);
+
+  let row = 0,
+    col = 0;
+
+  for (let i = 0; i < s.length; ) {
+    while (row < numRows && i < s.length) {
+      matrix[row][col] = s[i];
+      ++row, ++i;
+    }
+
+    ++col;
+    row -= 2;
+
+    while (row > 0 && i < s.length) {
+      matrix[row][col] = s[i];
+      --row, ++col, ++i;
+    }
+  }
+  return matrix.map((m) => m.join("")).join("");
 }
 console.log(convert("PAYPALISHIRING", 3));
