4964efd87137a5cdbf345da21c9484254ffd726c
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-15 15:08:19 +0600

    added optimum solution

--- src/add-to-array-form.ts
+++ src/add-to-array-form.ts
@@ -1,19 +1,34 @@
-export function addToArrayForm(num: number[], k: number): number[] {
-  const numStr = num.join("");
-  let sum = BigInt(numStr) + BigInt(k);
+// export function addToArrayForm(num: number[], k: number): number[] {
+//   const numStr = num.join("");
+//   let sum = BigInt(numStr) + BigInt(k);
 
-  const arr = [];
-  while (sum) {
-    arr.unshift(sum % 10n);
-    sum = sum / 10n;
+//   const arr = [];
+//   while (sum) {
+//     arr.unshift(sum % 10n);
+//     sum = sum / 10n;
+//   }
+//   return arr;
+// }
+
+export function addToArrayForm(num: number[], k: number): number[] {
+  let len = num.length;
+  const result = [];
+
+  while (--len >= 0 || k > 0) {
+    if (len >= 0) k += num[len]; // Note: Important condition because there could be k but the length of the array is finished. In that case we could end up NaN.
+
+    result.push(Math.trunc(k % 10));
+    k = Math.trunc(k / 10);
   }
-  return arr;
+
+  return result.reverse();
 }
 
+console.time();
 console.log(
   addToArrayForm(
     [1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3],
     516
   )
 );
-// [1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,5,7,9]
+console.timeEnd();
