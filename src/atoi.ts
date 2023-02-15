974aca9a1ec60e47eb481cf6f97c0bfbf1703545
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-15 19:13:59 +0600

    optimized

--- src/atoi.ts
+++ src/atoi.ts
@@ -1,8 +1,19 @@
+// export function myAtoi(s: string): number {
+//   const parsed = parseInt(s);
+//   if (parsed < -2147483648) return -2147483648;
+//   if (parsed > 2147483647) return 2147483647;
+//   return Number.isNaN(+parsed) ? 0 : parsed;
+// }
+
 export function myAtoi(s: string): number {
-  const parsed = parseInt(s);
-  if (parsed < -2147483648) return -2147483648;
-  if (parsed > 2147483647) return 2147483647;
-  return Number.isNaN(+parsed) ? 0 : parsed;
+  s = s.trim();
+  const number = s.match(/^[-+]?\d+/);
+  if (number) {
+    const integer = parseInt(number[0]);
+    if (integer > 2147483647) return 2147483647;
+    else if (integer < -2147483648) return -2147483648;
+    return integer;
+  }
+  return 0;
 }
-console.log(myAtoi("01"));
-// "words and 987"
+console.log(myAtoi("  78 "));
