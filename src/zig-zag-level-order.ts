f3ae7dc1c0c51e5a8990b9d3e010aa15ea8a0c0e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-19 18:33:52 +0600

    solved zigzag bfs

--- src/zig-zag-level-order.ts
+++ src/zig-zag-level-order.ts
@@ -10,14 +10,34 @@ class TreeNode {
 }
 
 export function zigzagLevelOrder(root: TreeNode | null): number[][] {
-  return;
+  if (!root) return [];
+  const res: number[][] = [];
+  const queue: TreeNode[] = [root];
+  let level = 0;
+
+  while (queue.length) {
+    const temp = [];
+    const len = queue.length;
+
+    for (let i = 0; i < len; i++) {
+      const current = queue.shift();
+      temp.push(current.val);
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    if (level % 2 === 1) temp.reverse();
+    res.push(temp);
+    ++level;
+  }
+  return res;
 }
 console.log(
   zigzagLevelOrder(
     new TreeNode(
-      3,
-      new TreeNode(9, null, null),
-      new TreeNode(20, new TreeNode(15), new TreeNode(7))
+      1,
+      new TreeNode(2, new TreeNode(4), null),
+      new TreeNode(3, null, new TreeNode(5))
     )
   )
 );
