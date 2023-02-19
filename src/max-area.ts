9ab727f703ae14e14f889636088643c16d1931bd
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-20 00:05:09 +0600

    added brute force solution

--- src/max-area.ts
+++ src/max-area.ts
@@ -1,4 +1,16 @@
+// Note: Brute Force
 export function maxArea(height: number[]): number {
-  return;
+  let initialWater = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const currentWater = Math.min(height[i], height[j]) * (j - i);
+      initialWater = Math.max(initialWater, currentWater);
+    }
+  }
+
+  return initialWater;
 }
-console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1, 2, 6, 2, 5, 4, 8, 3, 7]));
+
+// Note: Two Pointers
