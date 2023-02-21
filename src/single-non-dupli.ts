12fecbe267e9523425416b6e5c44877cfd199fb6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-21 18:09:37 +0600

    neat solution

--- src/single-non-dupli.ts
+++ src/single-non-dupli.ts
@@ -5,19 +5,12 @@ export function singleNonDuplicate(nums: number[]): number {
 
   while (left <= right) {
     mid = Math.floor((left + right) / 2);
+    if (mid % 2) mid -= 1;
 
-    if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1])
+    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1])
       return nums[mid];
-    if (
-      (mid % 2 !== 0 && nums[mid] === nums[mid + 1]) ||
-      (mid % 2 === 0 && nums[mid] === nums[mid - 1])
-    ) {
-      right = mid - 1;
-    } else if (
-      (mid % 2 === 0 && nums[mid] === nums[mid + 1]) ||
-      (mid % 2 !== 0 && nums[mid] === nums[mid - 1])
-    )
-      left = mid + 1;
+    if (nums[mid] === nums[mid + 1]) left = mid + 2;
+    else right = mid - 2;
   }
 }
-console.log(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]));
+console.log(singleNonDuplicate([1, 1, 2, 2, 3, 3, 4, 4, 8, 8, 9]));
