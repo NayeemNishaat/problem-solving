794b76717bce75d130ab7dcaa72acb9da388dfac
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-27 16:21:43 +0600

    implemented find unique

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,10 +1,6 @@
 function generic(nums: number[]) {
-  const set = new Set();
-
-  for (let i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) return true;
-    set.add(nums[i]);
-  }
-  return false;
+  let unique = 0;
+  for (let i = 0; i < nums.length; i++) unique ^= nums[i];
+  return unique;
 }
-console.log(generic([1, 2]));
+console.log(generic([1, 2, 2]));
