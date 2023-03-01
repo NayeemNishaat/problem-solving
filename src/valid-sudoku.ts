f8fc8e1e8f8aae82870809a5d6fab34daca28b03
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-01 17:27:17 +0600

    refactored

--- src/valid-sudoku.ts
+++ src/valid-sudoku.ts
@@ -1,28 +1,27 @@
 export function validSudoku(board: string[][]): boolean {
   const totalCells = board.length ** 2;
-
   const rows = new Map<number, Set<number>>();
-  const columns = new Map<number, Set<number>>();
+  const cols = new Map<number, Set<number>>();
   const boxes = new Map<number, Set<number>>();
 
   for (let i = 0; i < totalCells; i++) {
-    const row = Math.floor(i / 9);
-    const column = i % 9;
-    const box = Math.floor(row / 3) * 3 + Math.floor(column / 3);
-    const cell = Number(board[row][column]);
+    const row = ~~(i / 9);
+    const col = i % 9;
+    const box = ~~(row / 3) * 3 + ~~(col / 3);
+    const cell = +board[row][col];
 
     if (isNaN(cell)) continue;
 
     if (!rows.has(row)) rows.set(row, new Set<number>());
-    if (!columns.has(column)) columns.set(column, new Set<number>());
+    if (!cols.has(col)) cols.set(col, new Set<number>());
     if (!boxes.has(box)) boxes.set(box, new Set<number>());
 
     if (rows.get(row).has(cell)) return false;
-    if (columns.get(column).has(cell)) return false;
+    if (cols.get(col).has(cell)) return false;
     if (boxes.get(box).has(cell)) return false;
 
     rows.get(row).add(cell);
-    columns.get(column).add(cell);
+    cols.get(col).add(cell);
     boxes.get(box).add(cell);
   }
 
@@ -30,7 +29,7 @@ export function validSudoku(board: string[][]): boolean {
 }
 console.log(
   validSudoku([
-    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
     [".", "9", "8", ".", ".", ".", ".", "6", "."],
     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
