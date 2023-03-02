e687b40d31ba46ce0e0d5d3162dffcf6dd1d0de9
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-02 18:55:48 +0600

    unique approach

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,7 +1,18 @@
 function generic(s: string): any {
+  const map = new Map<string, number[]>();
+
   for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+    const curr = s[i];
+
+    if (map.has(curr)) map.set(curr, [1 + map.get(curr)[0], i]);
+    else map.set(curr, [1, i]);
   }
-  return -1;
+
+  const min = Math.min(
+    ...[...map.values()].filter((el) => el[0] === 1).map((el) => el[1])
+  );
+
+  if (min === Infinity) return -1;
+  else return min;
 }
-console.log(generic("leetcode"));
+console.log(generic("aacbb"));
