49912af24d1bde24eef1bbe53594196e24eeed65
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-03 18:15:04 +0600

    refactored

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -2,13 +2,10 @@ function generic(s: string, t: string): any {
   if (s.length !== t.length) return false;
 
   const map = new Map();
-  for (let i = 0; i < s.length; i++)
-    map.has(s[i]) ? map.set(s[i], map.get(s[i]) + 1) : map.set(s[i], 1);
-
-  for (let j = 0; j < t.length; j++)
-    if (map.get(t[j]) > 0) map.set(t[j], map.get(t[j]) - 1);
+  for (const val of s) map.set(val, (map.get(val) || 0) + 1);
+  for (const val of t)
+    if (map.get(val) > 0) map.set(val, map.get(val) - 1);
     else return false;
-
   return true;
 }
-console.log(generic("aacc", "ccac"));
+console.log(generic("aacc", "caac"));
