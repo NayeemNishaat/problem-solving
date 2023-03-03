3808672cc6c51a1cecda8995b99802e8d9957edd
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-03 16:50:15 +0600

    solved valid anagram

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,4 +1,14 @@
-function generic(haystack: string, needle: string): any {
-  return haystack.indexOf(needle);
+function generic(s: string, t: string): any {
+  if (s.length !== t.length) return false;
+
+  const map = new Map();
+  for (let i = 0; i < s.length; i++)
+    map.has(s[i]) ? map.set(s[i], map.get(s[i]) + 1) : map.set(s[i], 1);
+
+  for (let j = 0; j < t.length; j++)
+    if (map.get(t[j]) > 0) map.set(t[j], map.get(t[j]) - 1);
+    else return false;
+
+  return true;
 }
-console.log(generic("oraorangeorangy", "oran"));
+console.log(generic("aacc", "ccac"));
