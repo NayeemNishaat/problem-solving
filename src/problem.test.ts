92ff1acfb960b7c7ebf92abadf3a6a438b32c512
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-03 16:25:28 +0600

    one line solution

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,11 +1,4 @@
 function generic(haystack: string, needle: string): any {
-  let j = 0,
-    k = 0;
-  for (let i = 0; i < haystack.length; ) {
-    if (haystack[k] === needle[j]) j++, k++;
-    else (j = 0), (k = ++i);
-    if (j === needle.length) return i;
-  }
-  return -1;
+  return haystack.indexOf(needle);
 }
 console.log(generic("oraorangeorangy", "oran"));
