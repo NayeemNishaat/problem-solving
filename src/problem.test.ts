67f9837a8d6419cbe17db8fa70792bdc37ca2f3a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-05 01:16:10 +0600

    working on permutation

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,44 +1,29 @@
-function generic(num: number): any {
-  const numStr = num.toString();
-  const len = ~~numStr.length / 2;
+function generic(array: number[]): any {
+  array.sort((a, b) => a - b);
+  let perms = [array.slice()];
 
-  const permutations = (array) => {
-    // Sort the input
-    array.sort();
-    // Add a copy and initialize our list of permutations
-    let perms = [array.slice()];
+  while (array) {
+    let [i, j, k] = Array(3).fill(array.length - 1);
 
-    while (array) {
-      let [i, j, k] = Array(3).fill(array.length - 1);
+    // Find the first non-increasing element
+    while (i > 0 && array[i - 1] >= array[i]) i--;
+    if (i <= 0) break;
+    // console.log(array[i - 1], array[i]);
+    // Find first element larger
+    while (array[j] <= array[i - 1]) j--;
 
-      // Find the first non-increasing element
-      while (i > 0 && array[i - 1] >= array[i]) i--;
-      // If we don't find one, we're done!
-      if (i <= 0) break;
-
-      // Find first element larger
-      while (array[j] <= array[i - 1]) j--;
-
-      // Swap them
-      [array[i - 1], array[j]] = [array[j], array[i - 1]];
-
-      // Reverse the suffix
-      while (i < k) {
-        [array[i], array[k]] = [array[k], array[i]];
-        i++;
-        k--;
-      }
-      // Add a copy of the current state of the array to the list of permutations
-      perms.push(array.slice());
+    // Swap them
+    [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    console.log(array, 8);
+    // Reverse the suffix
+    while (i < k) {
+      [array[i], array[k]] = [array[k], array[i]];
+      i++;
+      k--;
     }
-    return perms;
-  };
-  const perm = permutations([...numStr]);
-
-  return perm
-    .map((d) => {
-      return +d.slice(0, len).join("") + +d.slice(len).join("");
-    })
-    .sort((a, b) => a - b)[0];
+    // Add a copy of the current state of the array to the list of permutations
+    perms.push(array.slice());
+  }
+  return perms;
 }
-console.log(generic(678));
+console.log(generic([3, 1, 2]));
