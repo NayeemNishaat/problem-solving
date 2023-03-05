a0c3441230768fd3804fe1f5ad769e1848510b82
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-05 23:12:54 +0600

    solved longest common prefix

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,29 +1,8 @@
-function generic(array: number[]): any {
-  array.sort((a, b) => a - b);
-  let perms = [array.slice()];
+function generic(strs: string[]): any {
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++)
+    while (!strs[i].startsWith(prefix)) prefix = prefix.slice(0, -1);
 
-  while (array) {
-    let [i, j, k] = Array(3).fill(array.length - 1);
-
-    // Find the first non-increasing element
-    while (i > 0 && array[i - 1] >= array[i]) i--;
-    if (i <= 0) break;
-    // console.log(array[i - 1], array[i]);
-    // Find first element larger
-    while (array[j] <= array[i - 1]) j--;
-
-    // Swap them
-    [array[i - 1], array[j]] = [array[j], array[i - 1]];
-    console.log(array, 8);
-    // Reverse the suffix
-    while (i < k) {
-      [array[i], array[k]] = [array[k], array[i]];
-      i++;
-      k--;
-    }
-    // Add a copy of the current state of the array to the list of permutations
-    perms.push(array.slice());
-  }
-  return perms;
+  return prefix;
 }
-console.log(generic([3, 1, 2]));
+console.log(generic(["flower", "flow", "flight"]));
