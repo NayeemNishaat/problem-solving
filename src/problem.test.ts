7d72148ff5d9f128cbacef746f23e8dd324e31d1
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-07 16:01:06 +0600

    implemented reversing ll

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,2 +1,24 @@
-function generic() {}
-console.log(generic());
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const head = new ListNode(
+  1,
+  new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+);
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev = null;
+  while (head) {
+    const temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
+  }
+  return prev;
+}
+console.log(reverseList(head));
