d4a82d653cd5b0c355b63812caec171e726c5bd2
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-09 17:18:17 +0600

    implemented max depth

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,23 +1,38 @@
-class ListNode {
+class TreeNode {
   val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
     this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
 }
-const head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(1))));
-console.log(head);
 
-function hasCycle(head: ListNode | null): boolean {
-  let slow = head,
-    fast = head;
+const tree = new TreeNode(
+  3,
+  new TreeNode(9),
+  new TreeNode(20, new TreeNode(15), new TreeNode(7))
+);
 
-  while (fast) {
-    slow = slow.next;
-    fast = fast.next?.next;
-    if (slow === fast) return true;
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  let i = 1,
+    max = 1;
+
+  function dfs(root: TreeNode) {
+    if (root.left) {
+      max = Math.max(max, ++i);
+      dfs(root.left);
+    }
+    if (root.right) {
+      max = Math.max(max, ++i);
+      dfs(root.right);
+    }
+    i--;
   }
-  return false;
+  dfs(root);
+  return max;
 }
-console.log(hasCycle(head));
+console.log(maxDepth(tree));
