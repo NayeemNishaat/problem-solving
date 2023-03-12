ffa99e98eac79966b0d12d5dfd51704c550d0118
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-13 00:28:50 +0600

    implemented array to bst

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -15,7 +15,17 @@ const tree = new TreeNode(
   new TreeNode(2, new TreeNode(4), new TreeNode(3))
 );
 
-function sortedArrayToBST(nums: number[]): TreeNode | null {
-  return;
+function sortedArrayToBST(nums: number[]): any {
+  function bst(nums: number[], left: number, right: number) {
+    if (left > right) return null;
+
+    const mid = Math.trunc((left + right) / 2);
+    const root = new TreeNode(nums[mid]);
+    root.left = bst(nums, left, mid - 1);
+    root.right = bst(nums, mid + 1, right);
+    return root;
+  }
+
+  return bst(nums, 0, nums.length - 1);
 }
 console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
