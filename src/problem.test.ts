07b1d9ba43fbfc2c693058e050479b1a4c05bb11
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-14 17:17:47 +0600

    solved first bad version

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,31 +1,18 @@
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+// Suppose these are versions : true true true true true [false] false false false false false
+// Target : need to find the version which is false but next one true OR which is true but previous one is false
+// Aprroach : Binary Search
 
-const tree = new TreeNode(
-  1,
-  new TreeNode(2, new TreeNode(4), new TreeNode(3)),
-  new TreeNode(2, new TreeNode(4), new TreeNode(3))
-);
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    let left = 1,
+      right = n;
 
-function sortedArrayToBST(nums: number[]): any {
-  function bst(nums: number[], left: number, right: number) {
-    if (left > right) return null;
-
-    const mid = Math.trunc((left + right) / 2);
-    const root = new TreeNode(nums[mid]);
-    root.left = bst(nums, left, mid - 1);
-    root.right = bst(nums, mid + 1, right);
-    return root;
-  }
-
-  return bst(nums, 0, nums.length - 1);
-}
-console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
+    while (right >= left) {
+      const mid = Math.trunc((right + left) / 2);
+      if (left === right) return left;
+      if (isBadVersion(mid)) right = mid;
+      else left = mid + 1;
+    }
+    // return left;
+  };
+};
