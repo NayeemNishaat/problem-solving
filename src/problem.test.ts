495ad7c561b166c759ec3430484191ea684a3cfa
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-16 17:38:18 +0600

    solved for 1 and 3

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,9 +1,12 @@
 function climbStairs(n: number) {
-  n = ++n;
+  const hash = {};
   function fib(n: number) {
-    if (n < 2) return n;
-    return fib(n - 1) + fib(n - 2);
+    if (n < 0) return 0;
+    else if (n === 0) return 1;
+
+    if (hash[n]) return hash[n];
+    else return (hash[n] = fib(n - 1) + fib(n - 3));
   }
   return fib(n);
 }
-console.log(climbStairs(5));
+console.log(climbStairs(6)); // 1/3 steps each time
