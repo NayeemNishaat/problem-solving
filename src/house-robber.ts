c2e4c2588a1f9b7bf299725dd5f144a69e5b30d3
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-18 21:33:22 +0600

    solved robber with iterative approach

--- src/house-robber.ts
+++ src/house-robber.ts
@@ -1,9 +1,24 @@
 export function rob(nums: number[]): number {
   const hash = {};
   function recurse(nums: number[], len: number) {
-    if (len <= 0) return 0;
-    return Math.max(recurse(nums, len - 2) + nums[len], recurse(nums, len - 1));
+    if (len < 0) return 0;
+    if (hash[len] !== undefined) return hash[len];
+
+    const max = Math.max(
+      recurse(nums, len - 2) + nums[len],
+      recurse(nums, len - 1)
+    );
+    hash[len] = max;
+    return max;
   }
+
   return recurse(nums, nums.length - 1);
 }
-console.log(rob([1, 3, 1, 3, 100]));
+console.log(
+  rob([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ])
+);
--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,40 +1,13 @@
 function rob(nums: number[]): number {
-  const hash = {};
-  function recurse(nums: number[], len: number) {
-    if (len < 0) return 0;
-    if (hash[len] !== undefined) return hash[len];
+  let prev = 0,
+    max = 0;
 
-    const max = Math.max(
-      recurse(nums, len - 2) + nums[len],
-      recurse(nums, len - 1)
-    );
-    hash[len] = max;
-    return max;
+  for (let i = 0, n = nums.length; i < n; i++) {
+    const temp = max;
+    const currentMax = prev + nums[i];
+    if (currentMax > max) max = currentMax;
+    prev = temp;
   }
-
-  return recurse(nums, nums.length - 1);
+  return Math.max(prev, max);
 }
-console.log(
-  rob([
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-  ])
-);
-
-// function rob(nums: number[]): number {
-//   let prev = 0, max = 0;
-
-//   for (let i = 0, n = nums.length; i < n; i++) {
-//       const temp = max;
-//       const currentMax = prev + nums[i];
-
-//       if (currentMax > max)
-//           max = currentMax;
-
-//       prev = temp;
-//   }
-
-//   return prev > max ? prev : max;
-// }
+console.log(rob([1, 2, 3, 1]));
