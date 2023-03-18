cac8a90b72c5fe3b8e15f1ba69357cdd57223c24
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-18 21:27:06 +0600

    implemented house robber recursive

---
+++ src/house-robber.ts
@@ -0,0 +1,9 @@
+export function rob(nums: number[]): number {
+  const hash = {};
+  function recurse(nums: number[], len: number) {
+    if (len <= 0) return 0;
+    return Math.max(recurse(nums, len - 2) + nums[len], recurse(nums, len - 1));
+  }
+  return recurse(nums, nums.length - 1);
+}
+console.log(rob([1, 3, 1, 3, 100]));
--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,10 +1,40 @@
 function rob(nums: number[]): number {
   const hash = {};
   function recurse(nums: number[], len: number) {
-    if (len <= 0) return 0;
-    //
-    return Math.max(recurse(nums, len - 2) + nums[len], recurse(nums, len - 1));
+    if (len < 0) return 0;
+    if (hash[len] !== undefined) return hash[len];
+
+    const max = Math.max(
+      recurse(nums, len - 2) + nums[len],
+      recurse(nums, len - 1)
+    );
+    hash[len] = max;
+    return max;
   }
+
   return recurse(nums, nums.length - 1);
 }
-console.log(rob([1, 3, 1, 3, 100]));
+console.log(
+  rob([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ])
+);
+
+// function rob(nums: number[]): number {
+//   let prev = 0, max = 0;
+
+//   for (let i = 0, n = nums.length; i < n; i++) {
+//       const temp = max;
+//       const currentMax = prev + nums[i];
+
+//       if (currentMax > max)
+//           max = currentMax;
+
+//       prev = temp;
+//   }
+
+//   return prev > max ? prev : max;
+// }
