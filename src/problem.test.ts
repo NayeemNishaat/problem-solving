fb2cb34eeadbb2da03cd6f94ce2cff97cacb9f2f
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-19 20:59:18 +0600

    solved shuffle an array

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,33 +1,26 @@
-function getDigit(num, i) {
-  // return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-  return Math.trunc(Math.abs(num) / 10 ** i) % 10; // Important: Math.abs() to get positive number when we put negative number
-}
-// console.log(getDigit(235436, 2));
-
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.trunc(Math.log10(Math.abs(num))) + 1;
-}
-// console.log(digitCount(235436));
-
-function mostDigits(nums) {
-  let maxDigits = 0;
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+class Solution {
+  nums: number[] = [];
+  constructor(nums: number[]) {
+    this.nums = nums;
   }
-  return maxDigits;
-}
-// console.log(mostDigits([23, 567, 89, 977446, 90]));
 
-function radixSort(nums) {
-  const maxDigits = mostDigits(nums);
-  for (let i = 0; i < maxDigits; i++) {
-    const buckets = Array.from({ length: 10 }, () => []);
-    for (let j = 0; j < nums.length; j++)
-      buckets[getDigit(nums[j], i)].push(nums[j]);
-
-    nums = [].concat(...buckets);
+  reset(): number[] {
+    return this.nums;
   }
-  return nums;
+
+  shuffle(): number[] {
+    const nums = this.nums.slice();
+    for (let i = 0; i < nums.length; i++) {
+      const idx = Math.floor(Math.random() * nums.length) as number;
+      [nums[idx], nums[i]] = [nums[i], nums[idx]];
+    }
+    return nums;
+  }
 }
-console.log(radixSort([657, 2, 454, 5, 476, 454, 43, 79, 7, 56, 90, 0, 87]));
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
