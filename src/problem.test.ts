bb8e7e5ef454f2eeebbcfe155ce84c4a79539ee8
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-22 17:29:08 +0600

    implemented count prime

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,13 +1,9 @@
-function fizzBuzz(n: number): string[] {
-  const answer: string[] = [];
+function countPrimes(n: number): number {
+  const arr = Array.from({ length: n }, () => true);
+  arr[0] = arr[1] = false;
+  for (let i = 2; i * i < n; i++)
+    if (arr[i]) for (let j = i * i; j < n; j += i) arr[j] = false;
 
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) answer.push("FizzBuzz");
-    else if (i % 3 === 0) answer.push("Fizz");
-    else if (i % 5 === 0) answer.push("Buzz");
-    else answer.push(`${i}`);
-  }
-
-  return answer;
+  return arr.reduce((acc, el) => (el ? ++acc : acc), 0);
 }
-console.log(fizzBuzz(5));
+console.log(countPrimes(10));
