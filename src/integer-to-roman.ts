83525d4d8ac7074c615f2d54729019ddcde074a2
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-24 15:41:32 +0600

    implemented roman to int

--- src/integer-to-roman.ts
+++ src/integer-to-roman.ts
@@ -85,3 +85,24 @@ function intToRoman(num: number): string {
     return roman;
 };
 */
+
+export function romanToInt(s: string): number {
+  const hash = new Map(
+    Object.entries({
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    })
+  );
+  let num = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (hash.get(s[i]) < hash.get(s[i + 1])) num -= hash.get(s[i]);
+    else num += hash.get(s[i]);
+  }
+  return num;
+}
+console.log(romanToInt("III"));
--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,4 +1,20 @@
-function isPowerOfThree(n: number): boolean {
-  return (Math.log10(n) / Math.log10(3)) % 1 === 0;
+function romanToInt(s: string): number {
+  const hash = new Map(
+    Object.entries({
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    })
+  );
+  let num = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (hash.get(s[i]) < hash.get(s[i + 1])) num -= hash.get(s[i]);
+    else num += hash.get(s[i]);
+  }
+  return num;
 }
-console.log(isPowerOfThree(27));
+console.log(romanToInt("III"));
