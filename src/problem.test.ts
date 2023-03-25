669364c15e66b96734ae199c2a498d0f384daa52
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 00:13:26 +0600

    implemented hamming distance

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,10 +1,9 @@
-function hammingWeight(n: number): number {
+function hammingDistance(x: number, y: number): number {
   let count = 0;
-
-  for (let i = 0; i < 32; i++) {
-    count += n & 1;
-    n >>= 1;
-  }
+  const xor = (x ^ y).toString(2);
+  console.log(xor, (x ^ y).toString(2));
+  for (let i = 0; i < xor.length; i++) xor[i] === "1" && ++count;
   return count;
 }
-console.log(hammingWeight(0b00000000000000000000000000001011));
+console.log(hammingDistance(93, 73));
+console.log(2 ** 31);
