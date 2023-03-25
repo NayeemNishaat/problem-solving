6a52d107794e290378287f1d790637a3ad0b33f6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 00:42:45 +0600

    updated with bitmask

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,9 +1,9 @@
 function hammingDistance(x: number, y: number): number {
-  let count = 0;
-  const xor = (x ^ y).toString(2);
-  console.log(xor, (x ^ y).toString(2));
-  for (let i = 0; i < xor.length; i++) xor[i] === "1" && ++count;
+  let count = 0,
+    xor = x ^ y;
+
+  // for (let i = 0; i < xor.length; i++) xor[i] === "1" && ++count;
+  while (xor) (count += xor & 1), (xor >>= 1);
   return count;
 }
 console.log(hammingDistance(93, 73));
-console.log(2 ** 31);
