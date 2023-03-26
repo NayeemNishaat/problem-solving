b572083d9f3baae5ad11fe5ff1f669629a6a6619
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 16:08:56 +0600

    solved reverse bits

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,9 +1,9 @@
-function hammingDistance(x: number, y: number): number {
-  let count = 0,
-    xor = x ^ y;
-
-  // for (let i = 0; i < xor.length; i++) xor[i] === "1" && ++count;
-  while (xor) (count += xor & 1), (xor >>= 1);
-  return count;
+function reverseBits(n: number): number {
+  let ans = 0;
+  for (let i = 0; i < 32; i++) {
+    ans = (ans << 1) | (n & 1);
+    n >>= 1;
+  }
+  return ans;
 }
-console.log(hammingDistance(93, 73));
+console.log(reverseBits(0b00000010100101000001111010011100));
