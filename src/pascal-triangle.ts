05edda5c20be5da35d2b34540295879e66729757
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 18:13:21 +0600

    outsourced

---
+++ src/pascal-triangle.ts
@@ -0,0 +1,16 @@
+export function generate(numRows: number): number[][] {
+  const res: number[][] = [];
+  for (let i = 1; i <= numRows; i++) {
+    i === 1 && res.push([1]);
+    if (i > 1) {
+      const tempRes = [1];
+      for (let j = 0; j < i - 2; j++)
+        tempRes.push(res[i - 2][j] + res[i - 2][j + 1]);
+
+      tempRes.push(1);
+      res.push(tempRes);
+    }
+  }
+  return res;
+}
+console.log(generate(5));
