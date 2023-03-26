c016c86ea983efd36fe7cc69873368254512896e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 17:10:56 +0600

    Solved pascal's triangle

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,9 +1,16 @@
-function reverseBits(n: number): number {
-  let ans = 0b0;
-  for (let i = 0; i < 32; i++) {
-    ans = (ans << 1) | (n & 1);
-    n >>= 1;
+function generate(numRows: number): number[][] {
+  const res: number[][] = [];
+  for (let i = 1; i <= numRows; i++) {
+    i === 1 && res.push([1]);
+    if (i > 1) {
+      const tempRes = [1];
+      for (let j = 0; j < i - 2; j++) {
+        tempRes.push(res[i - 2][j] + res[i - 2][j + 1]);
+      }
+      tempRes.push(1);
+      res.push(tempRes);
+    }
   }
-  return ans >>> 0; // Note: Unsigned right shift
+  return res;
 }
-console.log(reverseBits(0b11111111111111111111111111111101));
+console.log(generate(5));
