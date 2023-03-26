0abfb93247a3fbdb4b969c06ddec2f61515094c1
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 16:22:09 +0600

    implemented reverse bits

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,9 +1,9 @@
 function reverseBits(n: number): number {
-  let ans = 0;
+  let ans = 0b0;
   for (let i = 0; i < 32; i++) {
     ans = (ans << 1) | (n & 1);
     n >>= 1;
   }
-  return ans;
+  return ans >>> 0; // Note: Unsigned right shift
 }
-console.log(reverseBits(0b00000010100101000001111010011100));
+console.log(reverseBits(0b11111111111111111111111111111101));
