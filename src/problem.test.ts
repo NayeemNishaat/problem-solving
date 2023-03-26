092b01800fe5c4b8d888c776435dc821b1d362a6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-26 17:14:10 +0600

    refactored

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -4,9 +4,9 @@ function generate(numRows: number): number[][] {
     i === 1 && res.push([1]);
     if (i > 1) {
       const tempRes = [1];
-      for (let j = 0; j < i - 2; j++) {
+      for (let j = 0; j < i - 2; j++)
         tempRes.push(res[i - 2][j] + res[i - 2][j + 1]);
-      }
+
       tempRes.push(1);
       res.push(tempRes);
     }
