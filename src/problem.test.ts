76e5e2aa1dd72d28937ef7aa6ba2005be3b74b63
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-03-28 17:11:52 +0600

    solved but tle

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,9 +1,14 @@
 function mincostTickets(days: number[], costs: number[]): number {
-  function helper(covered, cost) {
+  const hash = {};
+
+  function helper(covered: number, cost: number) {
     if (covered > days[days.length - 1]) return cost;
     covered = days.find((el) => el > covered);
     if (covered === undefined) return cost;
-    // console.log(covered, cost);
+    // if (hash[covered]) return hash[covered];
+    console.log(covered, cost);
+    hash[covered] = cost;
+
     return Math.min(
       helper(covered, costs[0] + cost),
       helper(covered + 6, costs[1] + cost),
@@ -12,4 +17,18 @@ function mincostTickets(days: number[], costs: number[]): number {
   }
   return helper(0, 0);
 }
-console.log(mincostTickets([1, 4, 6, 7, 8, 20], [2, 7, 15]));
+console.log(
+  mincostTickets(
+    [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
+    ],
+    [2, 7, 15]
+  )
+);
+
+// const arr = [];
+// for (let i = 0; i < 365; i++) {
+//   arr.push(i + 1);
+// }
+// console.log(arr.toString());
