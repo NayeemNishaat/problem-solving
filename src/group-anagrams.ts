f19973412d1b7bcd6b231f1b46035c78863e7c33
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-03 00:12:09 +0600

    updated

---
+++ src/group-anagrams.ts
@@ -0,0 +1,12 @@
+export function groupAnagrams(strs: string[]): string[][] {
+  const map = new Map();
+
+  for (const str of strs) {
+    const sorted = [...str].sort().join("");
+    if (map.has(sorted)) map.get(sorted).push(str);
+    else map.set(sorted, [str]);
+  }
+
+  return [...map.values()];
+}
+console.log(groupAnagrams(["ddg", "dgd", "", "", ""]));
