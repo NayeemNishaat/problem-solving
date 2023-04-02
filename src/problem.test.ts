6121a92c702809ce32e71947d43accfda81ca75a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-03 00:08:52 +0600

    cleaver approach

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,40 +1,12 @@
 function groupAnagrams(strs: string[]): string[][] {
-  const res = [];
-  const seen = {};
+  const map = new Map();
 
-  for (let i = 0; i < strs.length; i++) {
-    const temp = [strs[i]];
-    if (seen[strs[i]]) {
-      continue;
-    }
-
-    for (let j = 0; j < strs.length; j++) {
-      if (i === j) continue;
-      let a = true;
-      const ob = {};
-      if (strs[i].length !== strs[j].length) a = false;
-      else if (strs[i] && !strs[j]) a = false;
-      else {
-        for (let k = 0; k < strs[j].length; k++) {
-          ob[strs[i][k]] ? ob[strs[i][k]]++ : (ob[strs[i][k]] = 1);
-        }
-
-        for (let k = 0; k < strs[j].length; k++) {
-          if (!ob[strs[j][k]]) {
-            a = false;
-            break;
-          } else ob[strs[j][k]]--;
-        }
-      }
-
-      if (a) {
-        seen[strs[j]] = true;
-        seen[strs[i]] = true;
-        temp.push(strs[j]);
-      }
-    }
-    res.push(temp);
+  for (const str of strs) {
+    const sorted = [...str].sort().join("");
+    if (map.has(sorted)) map.get(sorted).push(str);
+    else map.set(sorted, [str]);
   }
-  return res;
+
+  return [...map.values()];
 }
-console.log(groupAnagrams(["ddg", "dgdt", "", "", ""]));
+console.log(groupAnagrams(["ddg", "dgd", "", "", ""]));
