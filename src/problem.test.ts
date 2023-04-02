954360a522300893be2366d311ace868a5563070
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-02 21:18:38 +0600

    solved group anagrams brute force

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -11,17 +11,21 @@ function groupAnagrams(strs: string[]): string[][] {
     for (let j = 0; j < strs.length; j++) {
       if (i === j) continue;
       let a = true;
+      const ob = {};
+      if (strs[i].length !== strs[j].length) a = false;
+      else if (strs[i] && !strs[j]) a = false;
+      else {
+        for (let k = 0; k < strs[j].length; k++) {
+          ob[strs[i][k]] ? ob[strs[i][k]]++ : (ob[strs[i][k]] = 1);
+        }
 
-      for (let k = 0; k < strs[j].length; k++) {
-        if (
-          !strs[i].includes(strs[j][k]) ||
-          strs[i].length !== strs[j].length
-        ) {
-          a = false;
-          break;
+        for (let k = 0; k < strs[j].length; k++) {
+          if (!ob[strs[j][k]]) {
+            a = false;
+            break;
+          } else ob[strs[j][k]]--;
         }
       }
-      if (!strs[j] && !!strs[i]) a = false;
 
       if (a) {
         seen[strs[j]] = true;
@@ -33,4 +37,4 @@ function groupAnagrams(strs: string[]): string[][] {
   }
   return res;
 }
-console.log(groupAnagrams(["ddg", "dgg"]));
+console.log(groupAnagrams(["ddg", "dgdt", "", "", ""]));
