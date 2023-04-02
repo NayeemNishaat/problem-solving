3e3976e579d106a2753bf4ccab31cf2300c09a82
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-02 20:37:43 +0600

    fixing issue

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -4,18 +4,24 @@ function groupAnagrams(strs: string[]): string[][] {
 
   for (let i = 0; i < strs.length; i++) {
     const temp = [strs[i]];
-    if (seen[strs[i]]) continue;
+    if (seen[strs[i]]) {
+      continue;
+    }
 
     for (let j = 0; j < strs.length; j++) {
-      if (i === j || seen[strs[j]]) continue;
+      if (i === j) continue;
       let a = true;
 
       for (let k = 0; k < strs[j].length; k++) {
-        if (!strs[i].includes(strs[j][k])) {
+        if (
+          !strs[i].includes(strs[j][k]) ||
+          strs[i].length !== strs[j].length
+        ) {
           a = false;
           break;
         }
       }
+      if (!strs[j] && !!strs[i]) a = false;
 
       if (a) {
         seen[strs[j]] = true;
@@ -27,4 +33,4 @@ function groupAnagrams(strs: string[]): string[][] {
   }
   return res;
 }
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams(["ddg", "dgg"]));
