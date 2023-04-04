13be9f6e7d9c463a87a2b17e18902e3f8b5b444b
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-04 13:46:18 +0600

    reimplemented

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,30 +1,26 @@
-function countVowelSubstrings(w: string): number {
+function solution(s: string) {
+  let count = 0,
+    left = 0,
+    right = 0,
+    uniqueVow = 0;
   const map = new Map();
-
-  let vow = 0,
-    j = 0,
-    k = 0,
-    cnt = 0;
-
   ["a", "e", "i", "o", "u"].forEach((v) => map.set(v, 0));
 
-  for (let i = 0; i < w.length; i++) {
-    if (map.has(w[i])) {
-      map.set(w[i], map.get(w[i]) + 1);
-      map.get(w[i]) === 1 && vow++;
-      while (vow === 5) {
-        map.set(w[k], map.get(w[k]) - 1);
-        map.get(w[k]) === 0 && vow--;
-        k++;
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+      if (map.get(s[i]) === 1) uniqueVow++;
+      while (uniqueVow === 5) {
+        map.set(s[right], map.get(s[right]) - 1);
+        if (map.get(s[right]) === 0) uniqueVow--;
+        right++;
       }
-      cnt += k - j;
+      count += right - left;
     } else {
       ["a", "e", "i", "o", "u"].forEach((v) => map.set(v, 0));
-      vow = 0;
-      j = i + 1;
-      k = i + 1;
+      (left = i + 1), (right = i + 1);
     }
   }
-  return cnt;
+  return count;
 }
-console.log(countVowelSubstrings("xxaiioueiiaxx"));
+console.log(solution("aeiouaa"));
