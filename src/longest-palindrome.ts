953bc870bbaa96553cd5f8bb5fdd3656e8eabbae
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-05 17:23:01 +0600

    updated palindromic substr

--- src/longest-palindrome.ts
+++ src/longest-palindrome.ts
@@ -30,3 +30,31 @@ export function longestPalindrome(s: string): string {
   return longest;
 }
 console.log(longestPalindrome("aa"));
+
+function solution(s: string) {
+  if (s.length < 2) return s;
+  let left = 0,
+    right = 0,
+    max = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i - 1] === s[i]) {
+      (right = i), (left = i - 1);
+      while (s[left] === s[right] && left >= 0 && right < s.length)
+        left--, right++;
+
+      if (max.length < right-- - left++) max = s.slice(left, right + 1);
+    }
+
+    if (s[i - 1] === s[i + 1]) {
+      (right = i + 1), (left = i - 1);
+      while (s[left] === s[right] && left >= 0 && right < s.length)
+        left--, right++;
+
+      if (max.length < right-- - left++) max = s.slice(left, right + 1);
+    }
+  }
+
+  return max ? max : s[0];
+}
+console.log(solution("abcddd"));
