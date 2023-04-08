59dd123a930779fcf19907d9c3d70ee5ec19ec0b
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-08 17:36:12 +0600

    solved increasing triplet

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,14 +1,13 @@
-function increasingTriplet(nums: number[]): boolean {
+function solution(nums: number[]) {
   if (nums.length < 3) return false;
   let min = Infinity,
     mid = Infinity;
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > mid) return true;
-    else if (nums[i] > min && nums[i] < mid) mid = nums[i];
-    else if (nums[i] < min) min = nums[i];
+    min = Math.min(min, nums[i]);
+    if (nums[i] > min) mid = Math.min(mid, nums[i]);
   }
   return false;
 }
-console.log(increasingTriplet([4, 5, 6777, 1, 2]));
-[20, 100, 10, 12, 5, 13];
+console.log(solution([10, 100, 20, 0, 0, 140]));
