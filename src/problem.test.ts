57aabbbc8864c8bcdcea0df6bcb244784d061a46
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-08 18:16:55 +0600

    solved increasing quad

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,13 +1,18 @@
 function solution(nums: number[]) {
   if (nums.length < 3) return false;
-  let min = Infinity,
-    mid = Infinity;
+  let tiny = Infinity,
+    small = Infinity,
+    midium = Infinity,
+    large = Infinity,
+    huge = Infinity;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > mid) return true;
-    min = Math.min(min, nums[i]);
-    if (nums[i] > min) mid = Math.min(mid, nums[i]);
+    if (nums[i] > large) huge = Math.min(huge, nums[i]);
+    if (nums[i] > midium) /*  large = Math.min(large, nums[i]); */ return true;
+    if (nums[i] > small) midium = Math.min(midium, nums[i]);
+    if (nums[i] > tiny) small = Math.min(small, nums[i]);
+    if (nums[i] < tiny) tiny = Math.min(tiny, nums[i]);
   }
-  return false;
+  return { tiny, small, midium, large, huge };
 }
-console.log(solution([10, 100, 20, 0, 0, 140]));
+console.log(solution([10, 100, 20, 0, 0, 140, 0, 0, 0, 1000]));
