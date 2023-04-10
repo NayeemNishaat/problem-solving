ec6f635de103273fd7c2dfff1e8c5baa5a7e7929
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-10 16:27:19 +0600

    dfs inorder iterative

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,7 +1,29 @@
-const arr = [];
-function solution(nums: number[]) {
-  if (!nums.length) return 0;
-  const num = nums.shift();
-  return num + solution(nums);
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
 }
-console.log(solution([3, 2, 1, 7]));
+
+const root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  const res = [],
+    stack = [];
+
+  while (stack.length || root) {
+    if (root) stack.push(root), (root = root.left);
+    else {
+      const currentNode = stack.pop();
+      res.push(currentNode.val);
+      root = currentNode.right;
+    }
+  }
+
+  return res;
+}
+console.log(inorderTraversal(root));
