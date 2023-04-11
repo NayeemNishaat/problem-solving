6342cb3649a311b0073ab124c7205ff013a1ce6a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-11 15:05:36 +0600

    implemented build tree

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -11,30 +11,21 @@ class TreeNode {
 
 const root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
 
-function zigzagLevelOrder(root: TreeNode | null): number[][] {
-  if (!root) return [];
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  if (!preorder.length || !inorder.length) return null;
 
-  const res = [],
-    queue = [root];
-  let lvl = 0;
+  const root = new TreeNode(preorder[0]);
+  const rootIndex = inorder.indexOf(preorder[0]);
 
-  while (queue.length) {
-    const len = queue.length;
-    const temp = [];
+  root.left = buildTree(
+    preorder.slice(1, rootIndex + 1),
+    inorder.slice(0, rootIndex)
+  );
+  root.right = buildTree(
+    preorder.slice(rootIndex + 1),
+    inorder.slice(rootIndex + 1)
+  );
 
-    for (let i = 0; i < len; i++) {
-      const currentNode = queue.shift();
-      temp.push(currentNode.val);
-
-      currentNode.left && queue.push(currentNode.left);
-      currentNode.right && queue.push(currentNode.right);
-    }
-
-    if (lvl % 2 === 1) temp.reverse();
-    res.push(temp);
-    lvl++;
-  }
-
-  return res;
+  return root;
 }
-console.log(zigzagLevelOrder(root));
+console.log(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]));
