21b8fb80e5a05c9d8a46f5b3f1022ea7f0b91198
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-12 16:50:24 +0600

    test added

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -20,3 +20,32 @@ console.log(removeStars("abb*cdfg*****x*"));
 console.log(removeStars("leet**cod*e"));
 // console.log(Array(10000).fill("fhh**j*we*").join(""));
 // console.log(Array(10000).fill("fhh**j*we*").join(""));
+
+// Test:
+// const map = new Map();
+// let count = 0,
+//   currentIndex = Infinity,
+//   ini = Infinity;
+// // Part: Count Contageous Stars
+// for (let i = 0; i < s.length; i++) {
+//   if (s[i] === "*" && s[i + 1] === "*") {
+//     count++;
+//     if (ini + 1 === i) map.set(currentIndex, count), ini++;
+//     else (currentIndex = i), (ini = i);
+//   } else if (s[i] === "*") {
+//     count++;
+//     if (ini + 1 === i) map.set(currentIndex, count), ini++;
+//     else map.set(i, count);
+//   } else count = 0;
+// }
+// // Part: Remove Items From String
+// let ns = "",
+//   b = 0;
+// map.forEach((v, k) => {
+//   ns = s.slice(b, k - v);
+//   ns += s.slice(k + v);
+//   // b = k + v;
+//   console.log(ns);
+// });
+// ns += s.slice(b);
+// return ns;
