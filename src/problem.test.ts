7e0a7edf70f9f0cabd121b6d538189cdf12e178d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-21 19:37:02 +0600

    working on islands

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,30 +1,57 @@
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+function numIslands(grid: string[][]): number {
+  let numIslands = 0;
+  const visited = new Map();
 
-const tree = new TreeNode(
-  3,
-  new TreeNode(1, null, new TreeNode(2)),
-  new TreeNode(4)
+  function bfs(i: number, j: number) {
+    const queue = [];
+    queue.push([i, j]);
+    visited.set(`${i}${j}`, true);
+
+    // if (grid[i - 1]?.[j] === "0" || grid[i - 1]?.[j] === undefined) temp++;
+    // if (grid[i + 1]?.[j] === "0" || grid[i + 1]?.[j] === undefined) temp++;
+    // if (grid[i][j - 1] === "0" || grid[i][j - 1] === undefined) temp++;
+    // if (grid[i][j + 1] === "0" || grid[i][j + 1] === undefined) temp++;
+
+    while (queue.length) {
+      const [r, c] = queue.shift();
+
+      const directions = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1]
+      ];
+
+      directions.forEach(([dr, dc]) => {
+        const row = r + dr;
+        const col = c + dc;
+
+        if (grid[row]?.[col] === "1" && !visited.has(`${row}${col}`)) {
+          queue.push([row, col]);
+          visited.set(`${row}${col}`, true);
+        }
+      });
+    }
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "1" && !visited.has(`${i}${j}`))
+        numIslands++, bfs(i, j);
+    }
+  }
+  return numIslands;
+}
+console.log(
+  numIslands([
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+  ])
 );
 
-function kthSmallest(root: TreeNode | null, k: number): number {
-  const vals = [];
-
-  function dfs(root: TreeNode) {
-    root.left && dfs(root.left);
-    vals.push(root.val);
-    root.right && dfs(root.right);
-  }
-  dfs(root);
-
-  return vals[k - 1];
-}
-console.log(kthSmallest(tree, 1));
+// console.dir(
+//   Array.from({ length: 300 }, () => [Math.round(Math.random()).toString()]),
+//   { maxArrayLength: null }
+// );
