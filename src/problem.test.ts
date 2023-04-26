0ce5c6a2fac3d575cab2bc616920ee95e453455a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-26 13:56:46 +0600

    implemented valid parenthesis

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,46 +1,17 @@
-function numIslands(grid: string[][]): number {
-  let numIslands = 0;
-  const map = new Map(),
-    directions = [
-      [1, 0],
-      [-1, 0],
-      [0, 1],
-      [0, -1]
-    ];
+function generateParenthesis(n: number): string[] {
+  const stack = [],
+    res = [];
 
-  function bfs(i: number, j: number) {
-    const queue = [[i, j]];
+  function backtrack(open: number, close: number) {
+    if (open === close && close === n && open === n)
+      return res.push(stack.join(""));
 
-    while (queue.length) {
-      const [r, c] = queue.shift();
-
-      directions.forEach(([dr, dc]) => {
-        const row = r + dr;
-        const col = c + dc;
-
-        if (grid[row]?.[col] === "1" && !map.has(`${row},${col}`)) {
-          queue.push([row, col]);
-          map.set(`${row},${col}`, true);
-        }
-      });
-    }
+    if (open < 3) stack.push("("), backtrack(open + 1, close), stack.pop();
+    if (close < open) stack.push(")"), backtrack(open, close + 1), stack.pop();
   }
+  backtrack(0, 0);
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1" && !map.has(`${i},${j}`)) {
-        bfs(i, j);
-        numIslands++;
-      }
-    }
-  }
-
-  return numIslands;
+  return res;
 }
 
-console.log(numIslands([["1", "0", "1", "1", "1"]]));
-
-// console.dir(
-//   Array.from({ length: 300 }, () => [Math.round(Math.random()).toString()]),
-//   { maxArrayLength: null }
-// );
+console.log(generateParenthesis(3));
