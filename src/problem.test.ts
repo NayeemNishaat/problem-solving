b23e66c12fcff6e6d19dfd845aca1f930462f792
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-30 23:13:28 +0600

    working with word search

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,17 +1,41 @@
-function solution(nums: number[]) {
-  const res = [];
-
-  function backtrack(nums: number[], prev: number[]) {
-    res.push([...prev]);
-
-    for (let i = 0; i < nums.length; i++) {
-      prev.push(nums[i]);
-      backtrack([...nums.slice(i + 1)], prev);
-      prev.pop();
-    }
+function exist(board: string[][], word: string): boolean {
+  let len = word.length;
+  function backtrack(i: number) {
+    if (i === len) return true;
+    board.forEach((el) => {
+      el.forEach((e) => {
+        if (e === word[i]) {
+          backtrack(i + 1);
+        }
+      });
+    });
   }
-  backtrack(nums, []);
+  return backtrack(0);
+  // function allPermutation(nums: number[]) {
+  //   const stack = [],
+  //     res = [],
+  //     len = nums.length;
 
-  return res;
+  //   function backtrack(nums: number[]) {
+  //     if (stack.length === len) return res.push(Array.from(stack));
+
+  //     for (let i = 0; i < nums.length; i++) {
+  //       stack.push(nums[i]);
+  //       backtrack([...nums.slice(0, i), ...nums.slice(i + 1)]);
+  //       stack.pop();
+  //     }
+  //   }
+  //   backtrack(nums);
+  //   return res;
+  // }
 }
-console.log(solution([1, 2, 3]));
+console.log(
+  exist(
+    [
+      ["A", "B", "C", "E"],
+      ["S", "F", "C", "S"],
+      ["A", "D", "E", "E"]
+    ],
+    "SEE"
+  )
+);
