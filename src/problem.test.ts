8c092ff4186087cf832f0caacead91145f476c60
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-01 03:13:01 +0600

    still working on issues

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,13 +1,12 @@
 function exist(board: string[][], word: string): boolean {
   let k = 0;
-  const map = new Map();
 
-  function backtrack(coords: number[]) {
+  function backtrack(coords: any[]) {
     const queue = [coords];
     let i = 0;
 
     while (queue.length) {
-      let [x, y, k] = queue.shift();
+      let [x, y, k, map] = queue.shift();
 
       i = k;
       if (k > word.length) return i;
@@ -28,9 +27,9 @@ function exist(board: string[][], word: string): boolean {
           board[X]?.[Y] === word[k] &&
           !map.has(`${X},${Y}`)
         ) {
-          console.log(map, X, Y);
-          queue.push([X, Y, k + 1]);
+          map = new Map(JSON.parse(JSON.stringify(Array.from(map))));
           map.set(`${X},${Y}`, true);
+          queue.push([X, Y, k + 1, map]);
         }
       });
     }
@@ -40,9 +39,10 @@ function exist(board: string[][], word: string): boolean {
 
   for (let i = 0; i < board.length; i++)
     for (let j = 0; j < board[i].length; j++)
-      if (board[i][j] === word[0] && !map.has(`${i},${j}`)) {
+      if (board[i][j] === word[0]) {
+        const map = new Map();
         map.set(`${i},${j}`, true);
-        k = backtrack([i, j, 1]);
+        k = Math.max(backtrack([i, j, 1, map]), k);
       }
 
   console.log(k);
@@ -55,6 +55,6 @@ console.log(
       ["a", "a", "a", "a"],
       ["a", "a", "a", "a"]
     ],
-    "aaaaaaa"
+    "aaaaaaaaaaaaa"
   )
 );
