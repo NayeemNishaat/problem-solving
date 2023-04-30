f86eb524249b15dc81a89fe6d5801d6e7922c968
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-04-30 23:42:47 +0600

    implementing backtrack

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,33 +1,36 @@
 function exist(board: string[][], word: string): boolean {
-  let len = word.length;
-  function backtrack(i: number) {
-    if (i === len) return true;
-    board.forEach((el) => {
-      el.forEach((e) => {
-        if (e === word[i]) {
-          backtrack(i + 1);
-        }
+  function backtrack(coords: number[]) {
+    console.log(coords);
+    const queue = [coords];
+
+    while (queue.length) {
+      let [x, y] = queue.shift();
+
+      const positions = [
+        [-1, 0],
+        [1, 0],
+        [0, 1],
+        [0, -1]
+      ];
+
+      positions.forEach(([dx, dy]) => {
+        const X = x + dx,
+          Y = y + dy;
+
+        console.log(board[X][Y], [X, Y]);
       });
-    });
+    }
   }
-  return backtrack(0);
-  // function allPermutation(nums: number[]) {
-  //   const stack = [],
-  //     res = [],
-  //     len = nums.length;
 
-  //   function backtrack(nums: number[]) {
-  //     if (stack.length === len) return res.push(Array.from(stack));
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === word[0]) {
+        backtrack([i, j]);
+      }
+    }
+  }
 
-  //     for (let i = 0; i < nums.length; i++) {
-  //       stack.push(nums[i]);
-  //       backtrack([...nums.slice(0, i), ...nums.slice(i + 1)]);
-  //       stack.pop();
-  //     }
-  //   }
-  //   backtrack(nums);
-  //   return res;
-  // }
+  return;
 }
 console.log(
   exist(
