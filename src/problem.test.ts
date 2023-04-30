424ea29da937051794e2a699a268aef38e6a6ad3
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-01 01:20:33 +0600

    partially solved

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,10 +1,14 @@
 function exist(board: string[][], word: string): boolean {
+  let k = 1;
+
   function backtrack(coords: number[]) {
-    console.log(coords);
     const queue = [coords];
+    let i = 0;
 
     while (queue.length) {
-      let [x, y] = queue.shift();
+      let [x, y, k] = queue.shift();
+      i = k;
+      if (k > word.length) return i;
 
       const positions = [
         [-1, 0],
@@ -16,21 +20,25 @@ function exist(board: string[][], word: string): boolean {
       positions.forEach(([dx, dy]) => {
         const X = x + dx,
           Y = y + dy;
-
-        console.log(board[X][Y], [X, Y]);
+        // console.log(X, Y, 7888, k);
+        if (board[X]?.[Y] !== undefined && board[X]?.[Y] === word[k]) {
+          queue.push([X, Y, k + 1]);
+        }
       });
     }
+
+    return i;
   }
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (board[i][j] === word[0]) {
-        backtrack([i, j]);
+        k = backtrack([i, j, 1]);
       }
     }
   }
 
-  return;
+  return k === word.length;
 }
 console.log(
   exist(
