d11e7b0ec33532799f5d472fc7b64c4c669421d1
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-01 16:22:15 +0600

    outsourced word game

---
+++ src/word-search.ts
@@ -0,0 +1,49 @@
+export function exist(board: string[][], word: string): boolean {
+  function dfs(r: number, c: number, i: number) {
+    if (i === word.length) return true;
+    if (board[r]?.[c] === undefined || board[r]?.[c] !== word[i]) return false;
+
+    const temp = board[r][c];
+    board[r][c] = "";
+    let found: boolean;
+
+    const positions = [
+      [-1, 0],
+      [1, 0],
+      [0, 1],
+      [0, -1]
+    ];
+
+    positions.forEach(([dr, dc]) => {
+      const row = r + dr,
+        col = c + dc;
+
+      found = found || dfs(row, col, i + 1);
+    });
+
+    // const found =
+    //   dfs(r + 1, c, i + 1) ||
+    //   dfs(r - 1, c, i + 1) ||
+    //   dfs(r, c + 1, i + 1) ||
+    //   dfs(r, c - 1, i + 1);
+
+    board[r][c] = temp;
+    return found;
+  }
+
+  for (let i = 0; i < board.length; i++)
+    for (let j = 0; j < board[i].length; j++)
+      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
+
+  return false;
+}
+console.log(
+  exist(
+    [
+      ["A", "B", "C", "E"],
+      ["S", "F", "E", "S"],
+      ["A", "D", "E", "E"]
+    ],
+    "ABCESEEEFS"
+  )
+);
