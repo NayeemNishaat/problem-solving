7de28a0c8e9c3894fd972e7125d555425b48952e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-01 14:54:53 +0600

    refactored

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,16 +1,8 @@
 function exist(board: string[][], word: string): boolean {
-  const map = new Map();
-
   function dfs(r: number, c: number, i: number) {
     if (i === word.length) return true;
-    if (
-      board[r]?.[c] === undefined ||
-      board[r]?.[c] !== word[i] ||
-      map.has(`${r},${c}`)
-    )
-      return false;
+    if (board[r]?.[c] === undefined || board[r]?.[c] !== word[i]) return false;
 
-    // map.set(`${r},${c}`, true);
     const temp = board[r][c];
     board[r][c] = "";
     const found =
@@ -20,15 +12,12 @@ function exist(board: string[][], word: string): boolean {
       dfs(r, c - 1, i + 1);
 
     board[r][c] = temp;
-    // map.delete(`${r},${c}`);
     return found;
   }
 
   for (let i = 0; i < board.length; i++)
     for (let j = 0; j < board[i].length; j++)
-      if (board[i][j] === word[0] && dfs(i, j, 0)) {
-        return true;
-      }
+      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
 
   return false;
 }
@@ -42,11 +31,3 @@ console.log(
     "ABCESEEEFS"
   )
 );
-// [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
-// ABCESEEEFS
-// [
-//   ["a", "a", "a", "a"],
-//   ["a", "a", "a", "a"],
-//   ["a", "a", "a", "a"]
-// ],
-// "aaaaaaaaaaaaa"
