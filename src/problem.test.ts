c24ed107ab4f5a95f1e82277d8baf5a14bc4e9ee
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-03 16:32:44 +0600

    implemented sort colors eff

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,18 +1,36 @@
 function sortColors(nums: number[]): void {
-  let red = 0,
-    white = 0,
-    blue = nums.length - 1;
+  // Note: Better Approach
+  // let red = 0,
+  //   white = 0,
+  //   blue = nums.length - 1;
 
-  while (white <= blue) {
-    if (nums[white] === 0) {
-      [nums[red], nums[white]] = [nums[white], nums[red]];
-      white++, red++;
-    } else if (nums[white] === 1) white++;
-    else {
-      [nums[white], nums[blue]] = [nums[blue], nums[white]];
-      blue--;
-    }
-  }
+  // while (white <= blue) {
+  //   if (nums[white] === 0) {
+  //     [nums[red], nums[white]] = [nums[white], nums[red]];
+  //     white++, red++;
+  //   } else if (nums[white] === 1) white++;
+  //   else {
+  //     [nums[white], nums[blue]] = [nums[blue], nums[white]];
+  //     blue--;
+  //   }
+  // }
+  // console.log(nums);
+
+  // Note: OK Approach
+  const map = new Map();
+
+  nums.forEach((el) =>
+    map.has(el) ? map.set(el, map.get(el) + 1) : map.set(el, 1)
+  );
+
+  let i = 0,
+    zeroes = map.get(0),
+    ones = map.get(1),
+    twos = map.get(2);
+  while (zeroes) (nums[i] = 0), i++, zeroes--;
+  while (ones) (nums[i] = 1), i++, ones--;
+  while (twos) (nums[i] = 2), i++, twos--;
+
   console.log(nums);
 }
 sortColors([2, 0, 2, 1, 1, 0]);
