ce2c5bd60104353834e4099e2e33e84ad4c4bcd0
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-08 17:38:04 +0600

    implemented search range

---
+++ src/search-range.ts
@@ -0,0 +1,26 @@
+export function searchRange(nums: number[], target: number): number[] {
+  let left = 0,
+    right = nums.length - 1,
+    mid = 0,
+    start = -1,
+    end = -1,
+    found = -1;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      found = mid;
+      break;
+    } else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  start = end = found;
+  if (found === -1) return [start, end];
+  while (nums[start] === target) start--;
+  while (nums[end] === target) end++;
+
+  return [++start, --end];
+}
+console.log(searchRange([], 0));
