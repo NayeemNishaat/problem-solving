93ba998fcd0942844f6dcc9fe0925ffc21fa7214
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-12 18:13:14 +0600

    solved

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,21 +1,20 @@
-export function merge(intervals: number[][]): number[][] {
-  intervals.sort((a, b) => a[0] - b[0]);
-  let res: number[][] = [intervals[0]];
+function search(nums: number[], target: number): number {
+  let left = 0,
+    right = nums.length - 1;
 
-  for (let i = 1; i < intervals.length; i++) {
-    const lastEl = res[res.length - 1];
-    const currEl = intervals[i];
-    if (currEl[0] <= lastEl[1]) lastEl[1] = Math.max(lastEl[1], currEl[1]);
-    else res.push(currEl);
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] >= nums[left]) {
+      if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+      else left = mid + 1;
+    } else {
+      if (target <= nums[right] && target > nums[mid]) left = mid + 1;
+      else right = mid - 1;
+    }
   }
-  return res;
+  return -1;
 }
 
-console.log(
-  merge([
-    [1, 4],
-    [4, 5],
-    [6, 7],
-    [0, 10]
-  ])
-);
+console.log(search([4, 5, 6, 7, 0, 1, 2], 10));
