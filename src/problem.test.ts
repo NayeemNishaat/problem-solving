37046420052feed1829cfe172a4a12dc15298dc2
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-14 17:00:57 +0600

    solved

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,39 +1,11 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-  let xLeft = 0,
-    yTop = 0,
-    xRight = matrix.length - 1,
-    yDown = matrix.length - 1,
-    xMid = 0,
-    yMid = 0;
-
-  while (xLeft <= xRight && yTop <= yDown) {
-    xMid = ~~((xLeft + xRight) / 2);
-    yMid = ~~((yTop + yDown) / 2);
-    console.log(
-      matrix[xMid][yMid],
-      { xMid, yMid, xLeft, xRight, yTop, yDown },
-      matrix[0][xMid],
-      matrix[yMid][0]
-    );
-    if (matrix[xMid][yMid] === target) return true;
-
-    if (matrix[xMid][yMid] > target) {
-      xRight = xMid - 1;
-      yDown = yMid - 1;
-    } else {
-      xLeft = xMid + 1;
-      yTop = yMid + 1;
-    }
-
-    // if (matrix[0][xMid] > target) {
-    //   xRight = xMid - 1;
-    // } else {
-    //   xLeft = xMid + 1;
-    // }
-    // if (matrix[yMid][0] > target) yDown = yMid - 1;
-    // else yTop = yMid + 1;
+  let top = 0,
+    right = matrix.length;
+  while (top < matrix.length && right >= 0) {
+    if (matrix[top][right] === target) return true;
+    if (matrix[top][right] < target) top++;
+    else right--;
   }
-
   return false;
 }
 console.log(
@@ -45,6 +17,6 @@ console.log(
       [10, 13, 14, 17, 24],
       [18, 21, 23, 26, 30]
     ],
-    7
+    19
   )
 );
