66401ccc4fb51bb595dd0020af98f04e86002d0d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-14 20:15:37 +0600

    alternative sln

--- src/search-matrix.ts
+++ src/search-matrix.ts
@@ -23,3 +23,16 @@ export function searchMatrix(matrix: number[][], target: number): boolean {
   return false;
 }
 console.log(searchMatrix([[-5]], -5));
+
+console.log(
+  searchMatrix(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30]
+    ],
+    -5
+  )
+);
