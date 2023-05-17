515bfe1682e1030d6eb6a1badb60c2d794266f53
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-17 13:13:15 +0600

    efficient approach

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,42 +1,10 @@
 function canJump(nums: number[]): boolean {
-  const memo = [];
+  let reach = 0;
 
-  function rec(i: number): boolean {
-    if (i >= nums.length - 1) return true;
-
-    if (memo[i] !== undefined) return memo[i];
-    for (let j = nums[i]; j > 0; j--) {
-      const res = rec(i + j);
-      memo[i] = res;
-      if (res) return true;
-    }
-    return (memo[i] = false);
+  for (let i = 0; i < nums.length; i++) {
+    if (reach < i) return false;
+    reach = Math.max(reach, i + nums[i]);
   }
-  return rec(0);
+  return true;
 }
 console.log(canJump([2, 5, 0, 0]));
-
-// class Solution {
-//   public:
-//       bool canJump(vector<int>& nums) {
-//           //it shows at max what index can I reach.
-//           //initially I can only reach index 0, hence reach = 0
-//           int reach = 0;
-
-//           for(int idx = 0; idx < nums.size(); idx++) {
-//               //at every index I'll check if my reach was atleast able to
-//               //reach that particular index.
-
-//               //reach >= idx -> great, carry on. Otherwise,
-//               if(reach < idx) return false;
-
-//               //now as you can reach this index, it's time to update your reach
-//               //as at every index, you're getting a new jump length.
-//               reach = max(reach, idx + nums[idx]);
-//           }
-
-//           //this means that you reached till the end of the array, wohooo!!
-//           return true;
-
-//       }
-//   };
