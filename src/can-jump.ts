569724d9a130a5a06b370a1fb3e4da4d0489d534
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-17 13:14:24 +0600

    updated

--- src/can-jump.ts
+++ src/can-jump.ts
@@ -1,17 +1,27 @@
 export function canJump(nums: number[]): boolean {
-  const memo = [];
+  let reach = 0;
 
-  function rec(i: number): boolean {
-    if (i >= nums.length - 1) return true;
-
-    if (memo[i] !== undefined) return memo[i];
-    for (let j = nums[i]; j > 0; j--) {
-      const res = rec(i + j);
-      memo[i] = res;
-      if (res) return true;
-    }
-    return (memo[i] = false);
+  for (let i = 0; i < nums.length; i++) {
+    if (reach < i) return false;
+    reach = Math.max(reach, i + nums[i]);
   }
-  return rec(0);
+  return true;
 }
 console.log(canJump([2, 5, 0, 0]));
+
+/* export function canJump(nums: number[]): boolean {
+    const memo = [];
+  
+    function rec(i: number): boolean {
+      if (i >= nums.length - 1) return true;
+  
+      if (memo[i] !== undefined) return memo[i];
+      for (let j = nums[i]; j > 0; j--) {
+        const res = rec(i + j);
+        memo[i] = res;
+        if (res) return true;
+      }
+      return (memo[i] = false);
+    }
+    return rec(0);
+  } */
