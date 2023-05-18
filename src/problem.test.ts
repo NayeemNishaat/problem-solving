22600f07fce650e34346626ad458528b39b123bf
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-18 15:26:36 +0600

    solved without memo

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,10 +1,13 @@
-function canJump(nums: number[]): boolean {
-  let reach = 0;
+function solution(num1: number, num2: number) {
+  const grid = Array.from({ length: num1 }, () => Array(num2).fill(1));
 
-  for (let i = 0; i < nums.length; i++) {
-    if (reach < i) return false;
-    reach = Math.max(reach, i + nums[i]);
+  function rec(x: number, y: number) {
+    if (x === num1 - 1 && y === num2 - 1) return 1;
+    if (grid[x]?.[y] === undefined) return 0;
+
+    return rec(x + 1, y) + rec(x, y + 1);
   }
-  return true;
+
+  return rec(0, 0);
 }
-console.log(canJump([2, 5, 0, 0]));
+console.log(solution(15, 15));
