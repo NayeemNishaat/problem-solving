f20cca127cc2b27746db06d43969aeb519b60a1d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-19 23:44:12 +0600

    found bug

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -6,38 +6,37 @@ function coinChange(coins: number[], amount: number): number {
   const map = new Map();
 
   function dp(total: number) {
-    if (total > amount) return arr;
+    if (total > amount) return;
     if (total === amount) {
       min = Math.min(min, arr.length);
 
       if (map.has(total) && map.get(total).length > arr.length) {
         map.set(total, Array.from(arr));
       }
-      return arr;
+      return;
     }
 
     if (map.has(total)) {
       if (map.get(total).length > arr.length) {
         map.set(total, Array.from(arr));
-      } else return arr;
+      } else return;
     }
 
-    let res = [];
     for (let i = 0; i < coins.length; i++) {
       arr.push(coins[i]);
 
       const total = arr.reduce((acc, curr) => acc + curr, 0);
-      res = dp(total);
+      dp(total);
       if (!map.has(total)) map.set(total, Array.from(arr));
       if (map.has(total) && map.get(total).length > arr.length) {
         map.set(total, Array.from(arr));
       }
       arr.pop();
     }
-    return arr;
   }
   dp(0);
   return min === Infinity ? -1 : min;
 }
-console.log(coinChange([186, 419, 83, 408], 6249));
+console.log(coinChange([3, 7, 405, 436], 8839));
 // [186, 419, 83, 408], 6249
+// [3,7,405,436], 8839
