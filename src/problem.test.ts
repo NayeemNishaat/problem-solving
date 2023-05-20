4ee1b3e0026fba2277900ad40de6c3a628c9460b
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-20 16:58:21 +0600

    fixing

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -2,41 +2,32 @@ function coinChange(coins: number[], amount: number): number {
   if (amount === 0) return 0;
 
   let min = Infinity;
-  let arr = [];
   const map = new Map();
 
-  function dp(total: number) {
-    if (total > amount) return;
-    if (total === amount) {
-      min = Math.min(min, arr.length);
-
-      if (map.has(total) && map.get(total).length > arr.length) {
-        map.set(total, Array.from(arr));
-      }
-      return;
+  function dp(amount: number, num: number) {
+    if (amount === 0) {
+      // console.log(amount, num);
+      min = Math.min(min, num);
+      return min;
     }
 
-    if (map.has(total)) {
-      if (map.get(total).length > arr.length) {
-        map.set(total, Array.from(arr));
-      } else return;
+    if (amount < 0) return Infinity;
+    if (map.has(amount)) {
+      // console.log(amount, num);
+      return map.get(amount);
     }
 
+    let res = Infinity;
     for (let i = 0; i < coins.length; i++) {
-      arr.push(coins[i]);
-
-      const total = arr.reduce((acc, curr) => acc + curr, 0);
-      dp(total);
-      if (!map.has(total)) map.set(total, Array.from(arr));
-      if (map.has(total) && map.get(total).length > arr.length) {
-        map.set(total, Array.from(arr));
-      }
-      arr.pop();
+      res = dp(amount - coins[i], num + 1);
+      map.set(amount, res);
     }
+    return res;
   }
-  dp(0);
+  dp(amount, 0);
   return min === Infinity ? -1 : min;
 }
-console.log(coinChange([3, 7, 405, 436], 8839));
-// [186, 419, 83, 408], 6249
-// [3,7,405,436], 8839
+console.log(coinChange([1, 3, 5, 4], 7));
+// [186, 419, 83, 408], 6249    20
+// [3,7,405,436], 8839     25
+// [1, 3, 4, 5], 7
