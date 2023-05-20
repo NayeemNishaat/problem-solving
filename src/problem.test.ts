1b29469c30d40575171d8fc83934e9d21790ac45
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-20 21:03:12 +0600

    solved

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -4,24 +4,25 @@ function coinChange(coins: number[], amount: number): number {
   let min = Infinity;
   const map = new Map();
 
-  function dp(amount: number, num: number) {
-    if (amount === 0) return num;
+  function dp(amount: number) {
+    if (amount === 0) return 0;
     if (amount < 0) return Infinity;
     if (map.has(amount)) return map.get(amount);
 
     let res = Infinity;
     for (let i = 0; i < coins.length; i++) {
-      res = dp(amount - coins[i], num + 1);
-      console.log(res);
+      res = dp(amount - coins[i]);
+
       min = Math.min(res, min);
     }
+    min = min === Infinity ? min : min + 1;
     map.set(amount, min);
-    return res;
+    return min;
   }
-  dp(amount, 0);
+  dp(amount);
   return min === Infinity ? -1 : min;
 }
-console.log(coinChange([1, 3, 4, 5], 7));
+console.log(coinChange([186, 419, 83, 408], 6249));
 // [186, 419, 83, 408], 6249    20
 // [3,7,405,436], 8839     25
 // [1, 3, 4, 5], 7
