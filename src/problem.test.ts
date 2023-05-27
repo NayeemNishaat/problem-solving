422fb2745e94745abe20610383065e988e7867c6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-28 02:05:25 +0600

    bug fixed

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -2,14 +2,11 @@ function lengthOfLIS(nums: number[]): number {
   const dp = Array.from({ length: nums.length }, () => 1);
 
   for (let i = nums.length - 2; i >= 0; i--) {
-    let max = 0;
     for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] <= nums[i]) continue;
-      max = Math.max(max, 1 + dp[j]);
+      if (nums[j] > nums[i]) dp[i] = Math.max(dp[i], 1 + dp[j]);
     }
-    dp[i] = max;
   }
-  return dp[0];
+  return Math.max(...dp);
 }
 
 console.log(lengthOfLIS([3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12]));
