18b6d8ea67bb51591c74bb79615966ab38a5f962
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-27 23:45:16 +0600

    bottom up approach added

--- src/lis.ts
+++ src/lis.ts
@@ -1,4 +1,5 @@
-export function lengthOfLIS(nums: number[]): number {
+// Chapter: Top Down
+/* export function lengthOfLIS(nums: number[]): number {
   const map = new Map(),
     stack = [];
 
@@ -17,8 +18,22 @@ export function lengthOfLIS(nums: number[]): number {
     return max;
   }
   return dp(-1);
-}
+} */
 
+// Chapter: Bottom Up
+export function lengthOfLIS(nums: number[]): number {
+  const dp = Array.from({ length: nums.length }, () => 1);
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    let max = 0;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] <= nums[i]) continue;
+      max = Math.max(max, 1 + dp[j]);
+    }
+    dp[i] = max;
+  }
+  return dp[0];
+}
 console.log(lengthOfLIS([0, 1, 0, 3, 2, 3]));
 // [10, 9, 2, 5, 3, 7, 101, 18]
 // [3,5,6,2,5,4,19,5,6,7,12]
--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,27 +1,18 @@
 function lengthOfLIS(nums: number[]): number {
-  const stack = [],
-    map = new Map();
+  const dp = Array.from({ length: nums.length }, () => 1);
 
-  function dp(idx: number) {
-    if (map.has(idx)) return map.get(idx);
-
+  for (let i = nums.length - 2; i >= 0; i--) {
     let max = 0;
-    for (let i = idx + 1; i < nums.length; i++) {
-      if (stack.at(-1) >= nums[i]) continue;
-
-      stack.push(nums[i]);
-      max = Math.max(max, dp(i) + 1);
-      stack.pop();
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] <= nums[i]) continue;
+      max = Math.max(max, 1 + dp[j]);
     }
-
-    map.set(idx, max);
-    return max;
+    dp[i] = max;
   }
-
-  return dp(-1);
+  return dp[0];
 }
 
-console.log(lengthOfLIS([0, 1, 0, 3, 2, 3]));
+console.log(lengthOfLIS([3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12]));
 // [10, 9, 2, 5, 3, 7, 101, 18]
 // [3,5,6,2,5,4,19,5,6,7,12]
 // [0,1,0,3,2,3]
