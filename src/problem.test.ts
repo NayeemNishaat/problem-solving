4ef225171761df01d2c8074d69afa4125f59940e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-11 21:13:17 +0600

    solved RandomizedSet

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,15 +1,22 @@
-export function lengthOfLIS(nums: number[]): number {
-  const dp = Array.from({ length: nums.length }, () => 1);
+class RandomizedSet {
+  dict = {};
+  constructor() {}
 
-  for (let i = nums.length - 2; i >= 0; i--) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] > nums[i]) dp[i] = Math.max(dp[i], 1 + dp[j]);
-    }
+  insert(val: number): boolean {
+    if (this.dict[val] === val) return false;
+    this.dict[val] = val;
+    return true;
   }
-  return Math.max(...dp);
-}
 
-console.log(lengthOfLIS([3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12]));
-// [10, 9, 2, 5, 3, 7, 101, 18]
-// [3,5,6,2,5,4,19,5,6,7,12]
-// [0,1,0,3,2,3]
+  remove(val: number): boolean {
+    if (this.dict[val] !== undefined) {
+      delete this.dict[val];
+      return true;
+    } else return false;
+  }
+
+  getRandom(): number {
+    const values: number[] = Object.values(this.dict);
+    return values[~~(Math.random() * values.length)];
+  }
+}
