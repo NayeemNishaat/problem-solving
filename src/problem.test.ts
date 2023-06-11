9ecc275da4d56fca5f706f334b2b9d64be85b494
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-11 21:17:14 +0600

    using map

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,22 +1,23 @@
 class RandomizedSet {
-  dict = {};
+  map = new Map();
+
   constructor() {}
 
   insert(val: number): boolean {
-    if (this.dict[val] === val) return false;
-    this.dict[val] = val;
+    if (this.map.get(val) === val) return false;
+    this.map.set(val, val);
     return true;
   }
 
   remove(val: number): boolean {
-    if (this.dict[val] !== undefined) {
-      delete this.dict[val];
+    if (this.map.has(val)) {
+      this.map.delete(val);
       return true;
     } else return false;
   }
 
   getRandom(): number {
-    const values: number[] = Object.values(this.dict);
+    const values: number[] = [...this.map.values()];
     return values[~~(Math.random() * values.length)];
   }
 }
