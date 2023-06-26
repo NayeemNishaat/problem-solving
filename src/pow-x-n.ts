6c57bafcd4ce13af78df967f0502b604745b3349
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-26 11:58:30 +0600

    updated pow(x,n)

--- src/pow-x-n.ts
+++ src/pow-x-n.ts
@@ -1,13 +1,12 @@
-export function myPow(x: number, n: number): number {
-  return n >= 0 ? qmi(x, n) : 1 / qmi(x, -n);
+function myPow(x: number, n: number): number {
+  return n >= 0 ? helper(x, n) : 1 / helper(x, -n);
 }
 
-function qmi(a: number, k: number): number {
+function helper(a: number, k: number): number {
   let res = 1;
   while (k) {
-    if (k & 1) {
-      res *= a;
-    }
+    if (k & 1) res *= a;
+
     a *= a;
     k >>>= 1;
   }
