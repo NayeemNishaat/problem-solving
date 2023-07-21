39e861cd2f212d532a99bd9c6f5de6ea2192ed2c
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-21 18:08:16 +0600

    updated

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,4 +1,9 @@
 function getSum(a: number, b: number): number {
-  return Math.log(Math.exp(a) * Math.exp(b));
+  while (b !== 0) {
+    let carry = a & b;
+    a = a ^ b;
+    b = carry << 1;
+  }
+
+  return a;
 }
-console.log(getSum(-8, 4));
