c27715bc1d9edf3cd8001c8f3110930c0c860f50
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-28 16:48:35 +0600

    solving task schedular

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,19 +1,43 @@
-function majorityElement(nums: number[]): number {
-  const map = new Map();
+function leastInterval(tasks: string[], n: number): number {
+  if (n === 0) return tasks.length;
 
-  nums.forEach((n) => {
-    if (map.has(n)) map.set(n, map.get(n) + 1);
-    else map.set(n, 1);
+  const map = new Map();
+  const lookupMap = new Map();
+
+  tasks.forEach((t) => {
+    if (map.has(t)) map.set(t, map.get(t) + 1);
+    else map.set(t, 1);
   });
 
-  const max = [-Infinity, -Infinity];
-  for (const [k, v] of map.entries()) {
-    if (v > max[1]) {
-      max[0] = k;
-      max[1] = v;
+  const options = [...map.keys()];
+  const res = [];
+  let i = 0;
+
+  while (map.size && res.length < 100) {
+    console.log(map, res, i, lookupMap);
+    if (
+      map.get(options[i]) !== 0 &&
+      (!lookupMap.has(options[i]) || lookupMap.get(options[i]) <= res.length)
+    ) {
+      res.push(options[i]);
+      lookupMap.set(options[i], res.length + n);
+      map.set(options[i], map.get(options[i]) - 1);
+      i++;
+      if (i === options.length) i = 0;
+    } else if (map.get(options[i]) === 0) {
+      map.delete(options[i]);
+      i++;
+      if (i === options.length) i = 0;
+    } else {
+      res.push("");
+      // i++;
+      // if (i === options.length) i = 0;
     }
   }
 
-  return max[0];
+  console.log(res);
+  return res.length;
 }
-majorityElement([1]);
+console.log(
+  leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2)
+);
