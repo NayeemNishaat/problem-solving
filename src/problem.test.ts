eac4c58635962a29f7a0292ac8dcdd07f63c5a77
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-28 18:45:35 +0600

    need to fix issue

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -15,35 +15,57 @@ function leastInterval(tasks: string[], n: number): number {
     else map.set(t, 1);
   });
 
-  const options = [...map.keys()];
-  const res = [];
-  let i = 0;
+  const map2 = new Map();
 
-  while (map.size && res.length < 100) {
-    if (
-      map.get(options[i]) &&
-      (!lookupMap.has(options[i]) || lookupMap.get(options[i]) <= res.length)
-    ) {
-      res.push(options[i]);
-      lookupMap.set(options[i], res.length + n);
-      map.set(options[i], map.get(options[i]) - 1);
-      i++;
-      if (i === options.length || lookupMap.get(options[0]) <= res.length)
-        i = 0;
-    } else if (map.get(options[i]) === 0) {
-      map.delete(options[i]);
-      options.splice(i, 1);
+  const helper = function (map, lookupMap, condition) {
+    const options = [...map.keys()];
+    const res = [];
+    let i = 0;
 
-      if (i === options.length) i = 0;
-    } else {
-      res.push("");
+    while (map.size) {
+      if (
+        map.get(options[i]) &&
+        (!lookupMap.has(options[i]) || lookupMap.get(options[i]) <= res.length)
+      ) {
+        res.push(options[i]);
+        lookupMap.set(options[i], res.length + n);
+        map.set(options[i], map.get(options[i]) - 1);
+        i++;
+        if (eval(condition)) i = 0;
+      } else if (map.get(options[i]) === 0) {
+        map.delete(options[i]);
+        options.splice(i, 1);
+
+        if (i === options.length) i = 0;
+      } else {
+        res.push("");
+      }
     }
-  }
-  console.log(res);
-  return res.length;
+    console.log(res.length);
+    return res.length;
+  };
+
+  const res1 = helper(map, lookupMap, "i === options.length");
+
+  lookupMap.clear();
+  tasks.forEach((t) => {
+    if (map2.has(t)) map2.set(t, map2.get(t) + 1);
+    else map2.set(t, 1);
+  });
+  const res2 = helper(
+    map2,
+    lookupMap,
+    "i === options.length || lookupMap.get(options[0]) <= res.length"
+  );
+
+  return Math.min(res1, res2);
 }
 console.log(
-  leastInterval(["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E"], 2)
+  // leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2)
+  leastInterval(
+    ["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E"],
+    36
+  )
 );
 
 // [
