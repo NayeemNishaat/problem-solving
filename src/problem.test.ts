64eb1353918d38b6f15d8e96f221f2a159e60d20
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-28 19:11:32 +0600

    trying to implement a solution

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,12 +1,6 @@
 function leastInterval(tasks: string[], n: number): number {
   if (n === 0) return tasks.length;
 
-  tasks.sort((a, b) => {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
-  });
-
   const map = new Map();
   const lookupMap = new Map();
 
@@ -15,55 +9,42 @@ function leastInterval(tasks: string[], n: number): number {
     else map.set(t, 1);
   });
 
-  const map2 = new Map();
+  let options = [...map.entries()];
+  const res = [];
+  let i = 0;
 
-  const helper = function (map, lookupMap, condition) {
-    const options = [...map.keys()];
-    const res = [];
-    let i = 0;
-
-    while (map.size) {
-      if (
-        map.get(options[i]) &&
-        (!lookupMap.has(options[i]) || lookupMap.get(options[i]) <= res.length)
-      ) {
-        res.push(options[i]);
-        lookupMap.set(options[i], res.length + n);
-        map.set(options[i], map.get(options[i]) - 1);
-        i++;
-        if (eval(condition)) i = 0;
-      } else if (map.get(options[i]) === 0) {
-        map.delete(options[i]);
-        options.splice(i, 1);
-
-        if (i === options.length) i = 0;
-      } else {
-        res.push("");
-      }
-    }
-    console.log(res.length);
-    return res.length;
-  };
-
-  const res1 = helper(map, lookupMap, "i === options.length");
-
-  lookupMap.clear();
-  tasks.forEach((t) => {
-    if (map2.has(t)) map2.set(t, map2.get(t) + 1);
-    else map2.set(t, 1);
+  options.sort((a, b) => {
+    if (a[1] < b[1]) return 1;
+    if (a[1] > b[1]) return -1;
+    return 0;
   });
-  const res2 = helper(
-    map2,
-    lookupMap,
-    "i === options.length || lookupMap.get(options[0]) <= res.length"
-  );
 
-  return Math.min(res1, res2);
+  options = options.map((el) => el[0]);
+
+  while (map.size) {
+    if (
+      map.get(options[i]) &&
+      (!lookupMap.has(options[i]) || lookupMap.get(options[i]) <= res.length)
+    ) {
+      res.push(options[i]);
+      lookupMap.set(options[i], res.length + n);
+      map.set(options[i], map.get(options[i]) - 1);
+      i++;
+      if (i === options.length) i = 0; // || lookupMap.get(options[0]) <= res.length
+    } else if (map.get(options[i]) === 0) {
+      map.delete(options[i]);
+      options.splice(i, 1);
+
+      if (i === options.length) i = 0;
+    } else {
+      res.push("");
+    }
+  }
+
+  console.log(res);
+  return res.length;
 }
 console.log(
   // leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2)
-  leastInterval(
-    ["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E"],
-    36
-  )
+  leastInterval(["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "E", "E"], 2)
 );
