c21dbdddd82aa55ca96214129c2bca4d1ffc7cfc
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-29 17:38:26 +0600

    solved last interval

--- src/least-interval.ts
+++ src/least-interval.ts
@@ -1,97 +1,96 @@
-export function leastInterval(tasks: string[], n: number): number {
+export class Node {
+  val: any;
+  priority: any;
+
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class priorityQueue {
+  values: any[];
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    const values = this.values;
+    values.push(newNode);
+    let idx = values.length - 1,
+      parentIdx = Math.floor((idx - 1) / 2);
+
+    while (values[idx].priority < values[parentIdx]?.priority) {
+      [values[idx], values[parentIdx]] = [values[parentIdx], values[idx]];
+
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+  }
+
+  dequeue() {
+    const vals = this.values;
+    [vals[0], vals[vals.length - 1]] = [vals[vals.length - 1], vals[0]];
+    const extracted = vals.pop();
+
+    let parentIdx = 0,
+      left = 2 * parentIdx + 1,
+      right = 2 * parentIdx + 2;
+
+    while (
+      vals[left]?.priority < vals[parentIdx]?.priority ||
+      vals[right]?.priority < vals[parentIdx]?.priority
+    ) {
+      if (!vals[right] || vals[left].priority < vals[right].priority)
+        ([vals[left], vals[parentIdx]] = [vals[parentIdx], vals[left]]),
+          (parentIdx = left);
+      else
+        ([vals[right], vals[parentIdx]] = [vals[parentIdx], vals[right]]),
+          (parentIdx = right);
+
+      (left = 2 * parentIdx + 1), (right = 2 * parentIdx + 2);
+    }
+    return extracted;
+  }
+}
+
+const heap = new priorityQueue();
+
+function leastInterval(tasks: string[], n: number): number {
   if (n === 0) return tasks.length;
 
-  tasks.sort((a, b) => {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
-  });
-
   const map = new Map();
-  const lookupMap = new Map();
+  const queue = [];
+  let time = 0;
 
   tasks.forEach((t) => {
     if (map.has(t)) map.set(t, map.get(t) + 1);
     else map.set(t, 1);
   });
 
-  const map2 = new Map();
+  for (const [k, v] of map.entries()) {
+    heap.enqueue(k, v * -1);
+  }
 
-  const helper = function (map, lookupMap, condition) {
-    const options = [...map.keys()];
-    const res = [];
-    let i = 0;
-
-    while (map.size) {
-      if (
-        map.get(options[i]) &&
-        (!lookupMap.has(options[i]) || lookupMap.get(options[i]) <= res.length)
-      ) {
-        res.push(options[i]);
-        lookupMap.set(options[i], res.length + n);
-        map.set(options[i], map.get(options[i]) - 1);
-        i++;
-        if (eval(condition)) i = 0;
-      } else if (map.get(options[i]) === 0) {
-        map.delete(options[i]);
-        options.splice(i, 1);
-
-        if (i === options.length) i = 0;
-      } else {
-        res.push("");
-      }
+  while (heap.values.length || queue.length) {
+    if (queue[0]?.[2] === time) {
+      const v = queue.shift();
+      heap.enqueue(v[0], v[1]);
     }
-    console.log(res.length);
-    return res.length;
-  };
 
-  const res1 = helper(map, lookupMap, "i === options.length");
+    const val = heap.dequeue();
+    if (val && val.priority + 1 !== 0) {
+      queue.push([val.val, val.priority + 1, time + n + 1]);
+    }
 
-  lookupMap.clear();
-  tasks.forEach((t) => {
-    if (map2.has(t)) map2.set(t, map2.get(t) + 1);
-    else map2.set(t, 1);
-  });
-  const res2 = helper(
-    map2,
-    lookupMap,
-    "i === options.length || lookupMap.get(options[0]) <= res.length"
-  );
+    time++;
+  }
 
-  return Math.min(res1, res2);
+  return time;
 }
+
 console.log(
-  // leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2)
-  leastInterval(
-    ["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E"],
-    36
-  )
+  // leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2) // 16
+  leastInterval(["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "E", "E"], 2) // 12
 );
-
-// [
-//   [1, 4],
-//   [5, 3],
-//   [10, 1]
-// ].sort((a, b) => {
-//   if (a[1] > b[1]) return 1;
-//   if (a[1] < b[1]) return -1;
-//   return 0;
-// });
-
-// ["A", "E", "B"].sort((a, b) => -1 * a.localeCompare(b));
-// ["A", "E", "B"].sort((a, b) => b.localeCompare(a));
-// users.sort((a, b) => a.firstname.replace(/\D/g, '').localeCompare(b.firstname.replace(/\D/g, '')));
-
-// const collator = new Intl.Collator("en-US");
-// function sortAsc(a, b) {
-//   if (typeof a === "string" && typeof b === "string") {
-//     return collator.compare(b, a);
-//   }
-//   return b - a;
-// }
-
-// tasks.sort((a, b) => {
-//   if (a < b) return -1;
-//   if (a > b) return 1;
-//   return 0;
-// });
