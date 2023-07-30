d0ab1ba2595266b75149e4e17448983329571b50
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-30 21:32:57 +0600

    solved Find Numbers with Even Number of Digits

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,17 +1,10 @@
-function leastInterval(tasks: string[], n: number): number {
-  const frequencies = new Map<string, number>();
+function findNumbers(nums: number[]): number {
+  nums = nums
+    .map((n) => {
+      return Math.max(Math.floor(Math.log10(Math.abs(n))), 0) + 1;
+    })
+    .filter((n) => n % 2 === 0);
 
-  for (const task of tasks)
-    frequencies.set(task, (frequencies.get(task) || 0) + 1);
-
-  const maxFrequency = Math.max(...frequencies.values());
-  const numberOfMaxFrequencies = [...frequencies.values()].reduce(
-    (acc, v) => (v === maxFrequency ? 1 : 0) + acc,
-    0
-  );
-
-  return Math.max(
-    (maxFrequency - 1) * (n + 1) + numberOfMaxFrequencies,
-    tasks.length
-  );
+  return nums.length;
 }
+findNumbers([555, 901, 482, 1771]);
