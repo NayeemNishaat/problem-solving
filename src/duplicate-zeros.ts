970d3c35ffefce4431d101d949bc6a460b7187b7
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-08-13 23:06:21 +0600

    outsourced

---
+++ src/duplicate-zeros.ts
@@ -0,0 +1,6 @@
+export function duplicateZeros(arr: number[]): void {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) if (arr[i] === 0) arr.splice(i, 0, 0), i++;
+  arr.splice(len);
+}
+console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
