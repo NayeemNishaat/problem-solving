16b2a1492396635a9fbddc2e2504e990d790a353
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-08-13 23:05:37 +0600

    refactored

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,13 +1,6 @@
 function duplicateZeros(arr: number[]): void {
-  const res = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) res.push(0, 0);
-    else res.push(arr[i]);
-  }
-
-  for (let i = 0; i < arr.length; i++) arr[i] = res[i];
-
-  console.log(arr);
+  const len = arr.length;
+  for (let i = 0; i < len; i++) if (arr[i] === 0) arr.splice(i, 0, 0), i++;
+  arr.splice(len);
 }
 console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
