6d33f2860571ed6687f9cd66be9758ac3e7bd45f
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-08-27 18:20:53 +0600

    alt approach

--- src/problem.test.ts
+++ src/problem.test.ts
@@ -1,20 +1,11 @@
 function removeElement(nums: number[], val: number): number {
-  let j = nums.length - 1,
-    c = 0;
-  for (let i = 0; i < nums.length && i <= j; i++) {
-    if (nums[i] === val) {
-      while (nums[j] === val) {
-        j--;
-        c++;
-      }
-
-      if (i > j) break;
-      [nums[i], nums[j]] = [nums[j], nums[i]];
-      j--;
-      c++;
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[j] = nums[i];
+      j++;
     }
   }
-  console.log(c);
-  return nums.length - c;
+  return j;
 }
-console.log(removeElement([1], 1));
+console.log(removeElement([1, 2, 2], 1));
