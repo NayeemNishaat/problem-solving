89776d26455b9699e85c3982fb036fa49e02ff08
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2024-06-20 18:16:41 +0600

    resolved max profit

--- src/buySellStock.ts
+++ src/buySellStock.ts
@@ -15,4 +15,17 @@ function maxProfit(prices: number[]): number {
 
   return max;
 }
+
+// function maxProfit(prices: number[]) {
+//   let min_price = prices[0],
+//     maxprof = 0;
+
+//   for (let i = 1; i < prices.length; i++) {
+//     maxprof = Math.max(maxprof, prices[i] - min_price);
+//     min_price = Math.min(prices[i], min_price);
+//   }
+
+//   return maxprof;
+// }
+
 console.log(maxProfit([7, 6, 4, 3, 1]));
--- src/exp.ts
+++ src/exp.ts
@@ -1,34 +1,34 @@
 export function main() {
-  function maxProfit(prices: number[]): number {
-    let currentProfit = 0,
-      max = 0,
-      left = 0,
-      right = 1;
+  // function maxProfit(prices: number[]): number {
+  //   let currentProfit = 0,
+  //     max = 0,
+  //     left = 0,
+  //     right = 1;
 
-    while (right < prices.length) {
-      currentProfit = prices[right] - prices[left];
+  //   while (right < prices.length) {
+  //     currentProfit = prices[right] - prices[left];
 
-      max += Math.max(currentProfit, max);
+  //     max = Math.max(currentProfit, max);
 
-      if (prices[right] < prices[left]) {
-        left = right;
-      }
-      right++;
-    }
-
-    return max;
-  }
+  //     if (prices[right] < prices[left]) {
+  //       left = right;
+  //     }
+  //     right++;
+  //   }
+  //   console.log(max, 33);
+  //   return max;
+  // }
   maxProfit([7, 1, 5, 3, 6, 4, 3, 12]);
 
-  // function maxProfit(prices: number[]) {
-  //   let min_price = prices[0],
-  //     maxprof = 0;
+  function maxProfit(prices: number[]) {
+    let min_price = prices[0],
+      maxprof = 0;
 
-  //   for (let i = 1; i < prices.length; i++) {
-  //     maxprof = Math.max(maxprof, prices[i] - min_price);
-  //     min_price = Math.min(prices[i], min_price);
-  //   }
+    for (let i = 1; i < prices.length; i++) {
+      maxprof = Math.max(maxprof, prices[i] - min_price);
+      min_price = Math.min(prices[i], min_price);
+    }
 
-  //   return maxprof;
-  // }
+    return maxprof;
+  }
 }
